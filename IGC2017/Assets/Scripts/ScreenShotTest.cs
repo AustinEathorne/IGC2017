c23b6505315912b2ab270fa464c1b2506973ccc0
AustinEathorne <austineathorne@gmail.com>
2017-10-13 19:06:36 +0200

    test some things....

---
+++ IGC2017/Assets/Scripts/ScreenShotTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScreenShotTest : MonoBehaviour 
+{
+
+	private string filePath = "Art/ScreenCaptures";
+	private string captureName = "test";
+
+	private int captureCount = 0;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log ("Capture");
+
+			string path = Application.persistentDataPath + "/" + (captureCount++).ToString() + "/" + "kjfbjkfbjk.png";
+
+			ScreenCapture.CaptureScreenshot (path);
+		}
+	}
+}
---
+++ IGC2017/Assets/Scripts/raycastTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastTest : MonoBehaviour {
+
+	[SerializeField]
+	private Transform cubeTransform;
+
+	Vector3 hitLocation;
+
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit))
+			{
+				hitLocation = ray.GetPoint (hit.distance);
+				Debug.Log (hitLocation);
+			}
+		}
+
+		cubeTransform.position = Vector3.MoveTowards(cubeTransform.position, hitLocation, Time.deltaTime * 5.0f);
+	}
+
+
+
+}

0f172d1a684f8974f574d611c8f2540b22d862c6
AustinEathorne <austineathorne@gmail.com>
2017-10-15 16:06:36 +0200

    Workin on the dude

---
+++ IGC2017/Assets/Scripts/MoveTheDude.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheDude : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed = 5.0f;
+
+	[SerializeField]
+	private float waitTime = 2.0f;
+
+	[SerializeField]
+	private Transform point1;
+
+	[SerializeField]
+	private Transform point2;
+
+
+
+	public void CallMove()
+	{
+		this.StartCoroutine (this.Move ());
+	}
+
+	private IEnumerator Move()
+	{
+		while(Vector3.Distance(this.transform.localPosition, point1.localPosition) >= 0.5f)
+		{
+			this.transform.localPosition = Vector3.MoveTowards (this.transform.localPosition, point1.transform.localPosition, Time.deltaTime * moveSpeed);
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(waitTime);
+
+		while(Vector3.Distance(this.transform.localPosition, point2.localPosition) >= 0.5f)
+		{
+			this.transform.localPosition = Vector3.MoveTowards (this.transform.localPosition, point2.transform.localPosition, Time.deltaTime * moveSpeed);
+			yield return null;
+		}
+
+		yield return null;
+	}
+}
